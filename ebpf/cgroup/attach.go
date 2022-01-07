f98ed04f963f805ff0b7fd93638812ab16aa4437
ZhengjunHUO <firelouiszj@hotmail.com>
2022-01-07 14:49:29 +0100

    update go programs

--- ebpf/cgroup/attach.go
+++ ebpf/cgroup/attach.go
@@ -12,14 +12,17 @@ import (
 )
 
 const (
-	cgroupPath	= "/sys/fs/cgroup/system.slice/docker-746823468eb932764abff0bc416aa39d96037d201976b293ccb66c10c4702567.scope"
+	//cgroupPath      = "/sys/fs/cgroup/system.slice/docker-746823468eb932764abff0bc416aa39d96037d201976b293ccb66c10c4702567.scope"
+	cgroupPath	= "/sys/fs/cgroup/system.slice/docker-5b81537a967793cf5c8b562bd5b9cb6b55045ed339ed328390f70d466aa84134.scope"
 
 	bpfProgName	= "bpf.o"
 	egressFuncName  = "egress_filter"
 	ingressFuncName = "ingress_filter"
 	egressMapName   = "egress_blacklist"
+	ingressMapName  = "ingress_blacklist"
 
-	linkPinPath     = "/sys/fs/bpf/cgroup_link"
+	egressLinkPinPath     = "/sys/fs/bpf/cgroup_egs_link"
+	ingressLinkPinPath    = "/sys/fs/bpf/cgroup_igs_link"
 )
 
 func main() {
@@ -33,19 +36,26 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	ingressFunc := collection.Programs[ingressFuncName]
 	egressFunc := collection.Programs[egressFuncName]
 
 	/* load map (temporary hardcode an entry to blacklist) */
 	egressMap := collection.Maps[egressMapName]
+	ingressMap := collection.Maps[ingressMapName]
 
-	ip := binary.LittleEndian.Uint32(net.ParseIP("8.8.4.4").To4())
+	ip_egs := binary.LittleEndian.Uint32(net.ParseIP("8.8.4.4").To4())
 	bTrue := true
-	if err = egressMap.Put(&ip, &bTrue); err != nil {
+	if err = egressMap.Put(&ip_egs, &bTrue); err != nil {
+		log.Fatalln(err)
+	}
+
+	ip_igs := binary.LittleEndian.Uint32(net.ParseIP("172.17.0.2").To4())
+	if err = ingressMap.Put(&ip_igs, &bTrue); err != nil {
 		log.Fatalln(err)
 	}
 
 	/* attach bpf program to specific cgroup */
-	l, err := link.AttachCgroup(link.CgroupOptions{
+	lnk_egs, err := link.AttachCgroup(link.CgroupOptions{
 		Path:    cgroupPath,
 		Attach:  ebpf.AttachCGroupInetEgress,
 		Program: egressFunc,
@@ -54,9 +64,21 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	lnk_igs, err := link.AttachCgroup(link.CgroupOptions{
+		Path:    cgroupPath,
+		Attach:  ebpf.AttachCGroupInetIngress,
+		Program: ingressFunc,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	/* pin link to the bpffs */
-	l.Pin(linkPinPath)
-	l.Close()
+	lnk_egs.Pin(egressLinkPinPath)
+	lnk_egs.Close()
+
+	lnk_igs.Pin(ingressLinkPinPath)
+	lnk_igs.Close()
 
 	fmt.Println("eBPF program attached.")
 }
--- ebpf/cgroup/detach.go
+++ ebpf/cgroup/detach.go
@@ -7,12 +7,13 @@ import (
 )
 
 const (
-	linkPinPath = "/sys/fs/bpf/cgroup_link"
+	egressLinkPinPath     = "/sys/fs/bpf/cgroup_egs_link"
+	ingressLinkPinPath    = "/sys/fs/bpf/cgroup_igs_link"
 )
 
 func main() {
 	// restore link from pinned file on bpffs
-	l, err := link.LoadPinnedCgroup(linkPinPath, nil)
+	l, err := link.LoadPinnedCgroup(egressLinkPinPath, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -20,5 +21,15 @@ func main() {
 	// remove the file on bpffs
 	l.Unpin()
 	l.Close()
+
+	l, err = link.LoadPinnedCgroup(ingressLinkPinPath, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// remove the file on bpffs
+	l.Unpin()
+	l.Close()
+
 	fmt.Println("Link unpinned")
 }

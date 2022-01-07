2cebcec2b65afd0c4e919bcb57ec459733284edd
ZhengjunHUO <firelouiszj@hotmail.com>
2022-01-07 14:46:12 +0100

    update bpf.c add ingress related code

--- ebpf/cgroup/bpf.c
+++ ebpf/cgroup/bpf.c
@@ -2,6 +2,13 @@
 #include <netinet/ip.h>
 #include "bpf_helpers.h"
 
+struct bpf_map_def SEC("maps") ingress_blacklist = {
+    .type = BPF_MAP_TYPE_HASH,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(_Bool),
+    .max_entries = 1000,
+};
+
 struct bpf_map_def SEC("maps") egress_blacklist = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = sizeof(__u32),
@@ -9,19 +16,20 @@ struct bpf_map_def SEC("maps") egress_blacklist = {
     .max_entries = 1000,
 };
 
-/*
 SEC("cgroup_skb/ingress")
 int ingress_filter(struct __sk_buff *skb) {
-    // Drop all
-    return 0;
+    struct iphdr iphd;
+    bpf_skb_load_bytes(skb, 0, &iphd, sizeof(struct iphdr));
+    bpf_printk("Ingress from %lu",iphd.saddr);
+
+    return !bpf_map_lookup_elem(&ingress_blacklist, &iphd.saddr);
 }
-*/
 
 SEC("cgroup_skb/egress")
 int egress_filter(struct __sk_buff *skb) {
     struct iphdr iphd;
     bpf_skb_load_bytes(skb, 0, &iphd, sizeof(struct iphdr));
-    //bpf_printk("Egress to %lu",iphd.daddr);
+    bpf_printk("Egress to %lu",iphd.daddr);
 
     return !bpf_map_lookup_elem(&egress_blacklist, &iphd.daddr);
 }
